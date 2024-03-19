b9ff76b6bc1341924b4ca4a676dcbfa255aae7d8
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-03-19 11:18:46 +0530

    m1 (folder 03 react basics and working ) video 10

---
+++ src/Component/ExpenseItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const ExpenseItem=()=>
+{
+  return(
+    <div>
+        <div>March 28th 2021</div>
+        <div>Car Insurances
+            <div>$294.69</div>
+        </div>
+    </div>
+  );
+}
+export default ExpenseItem;
