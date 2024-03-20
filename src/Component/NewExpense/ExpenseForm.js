efa77141eb550c247ee062400d3a759940bb2eee
sushmabhapkar <sushmabhapkar244@gmail.com>
2024-03-20 12:21:11 +0530

    m2 video 13

--- src/Component/NewExpense/ExpenseForm.js
+++ src/Component/NewExpense/ExpenseForm.js
@@ -33,6 +33,9 @@ const ExpenseForm=()=>
             date:enteredDate,
         };
         console.log(ExpenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     }
 
 return(
@@ -40,17 +43,32 @@ return(
     <div className="new-expense__controls">
         <div className="new-expense__control">
             <label>Title</label>
-            <input type="text" onChange={titleChangeHandler}/>
+            <input 
+                   type="text" 
+                   value={enteredTitle} 
+                   onChange={titleChangeHandler}
+            />
         </div>
         
         <div className="new-expense__control">
             <label>Amount</label>
-            <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
+            <input 
+               type="number" 
+               min="0.01" 
+               step="0.01" 
+               value={enteredAmount}
+               onChange={amountChangeHandler}
+            />
         </div>
 
         <div className="new-expense__control">
             <label>Date</label>
-            <input type="date" min="2019-1-1" max="2022-21-31" onChange={dateChangeHandler}/>
+            <input 
+                  type="date" 
+                  min="2019-1-1" 
+                  max="2022-21-31" 
+                  value={enteredDate}
+                  onChange={dateChangeHandler}/>
         </div>
         </div>
         
